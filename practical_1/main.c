b35ac7b5340c473e2ed6f2719d63cfde6902ecfc
Lithasa <lithasa0417@gmail.com>
2024-12-24 08:44:17 +0530

    Added practical 1

---
+++ practical_1/main.c
@@ -0,0 +1,173 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define pi 3.14159
+
+int main()
+{
+    printf("Name  :Erandhi Perera\n"
+           "DOB   :May 14,2000\n"
+           "Mobile:077-8854289\n\n");
+
+    printf("You are 10 years old. You are too young to play the game.\n\n");
+
+    printf("*****\n");
+    printf("*****\n");
+    printf("*****\n");
+    printf("*****\n");
+    printf("*****\n\n");
+
+    printf("######\n");
+    printf("#\n");
+    printf("#\n");
+    printf("#####\n");
+    printf("#\n");
+    printf("#\n");
+    printf("#\n\n");
+
+    printf(" ######\n");
+    printf("##   ##\n");
+    printf("#\n");
+    printf("#\n");
+    printf("#\n");
+    printf("#\n");
+    printf("#\n");
+    printf(" ##   ##\n");
+    printf("  ######\n\n");
+
+    printf("*********\n");
+    printf("*       *\n");
+    printf("*       *\n");
+    printf("*       *\n");
+    printf("*       *\n");
+    printf("*       *\n");
+    printf("*       *\n");
+    printf("*       *\n");
+    printf("*       *\n");
+    printf("*********\n\n");
+
+    printf("   ***   \n");
+    printf(" *     * \n");
+    printf("*       *\n");
+    printf("*       *\n");
+    printf("*       *\n");
+    printf("*       *\n");
+    printf("*       *\n");
+    printf(" *     * \n");
+    printf("   ***   \n\n");
+
+    printf("  *  \n");
+    printf(" *** \n");
+    printf("*****\n");
+    printf("  *  \n");
+    printf("  *  \n");
+    printf("  *  \n");
+    printf("  *  \n");
+    printf("  *  \n\n");
+
+    printf("    *    \n");
+    printf("   * *   \n");
+    printf("  *   *  \n");
+    printf(" *     * \n");
+    printf("*       * \n");
+    printf(" *     * \n");
+    printf("  *   *  \n");
+    printf("   * *   \n");
+    printf("    *    \n\n");
+
+    printf("LLLLLLLLLLLLLLL\n");
+    printf("L\n");
+    printf("L\n");
+    printf("L\n");
+    printf("L\n");
+    printf("L\n");
+    printf("         SS \n");
+    printf("S      S      S\n");
+    printf("S      S      S\n");
+    printf("S      S      S\n");
+    printf(" S     S      S\n");
+    printf("    SS      \n");
+
+    printf("A\n");
+    printf("    A\n");
+    printf("       A\n");
+    printf("       A   A\n");
+    printf("       A      A\n");
+    printf("       A         A\n");
+    printf("       A      A\n");
+    printf("       A   A\n");
+    printf("       A\n");
+    printf("    A\n");
+    printf("A\n\n");
+
+    printf("********\n");
+    printf(" ********\n");
+    printf("********\n");
+    printf(" ********\n");
+    printf("********\n");
+    printf(" ********\n");
+    printf("********\n");
+    printf(" ********\n\n");
+
+
+    int height,width,p,a;
+    height=7;
+    width=5;
+    p=(height+width)*2;
+    a=height*width;
+    printf("Perimeter is %d inches\n",p);
+    printf("Area is %d inches\n\n",a);
+
+    float radius,area,peri;
+    radius=6;
+    area=pi*radius*radius;
+    peri=2*pi*radius;
+
+    printf("The area of the circle is %f\n",area);
+    printf("The perimeter of the circle is %f\n\n",peri);
+
+    int n1,n2,n3,sum;
+    n1=5;
+    n2=6;
+    n3=2;
+    sum=n1+n2+n3;
+    printf("sum of 2,5,6 is =%d\n\n",sum);
+
+    int N1,N2,dif;
+    N1=10;
+    N2=4;
+    dif=N1-N2;
+
+    printf("Difference of 10 and 4 is = %d\n\n",dif);
+
+    int num1,num2,pro;
+    num1=20;
+    num2=15;
+    pro=num1*num2;
+
+    printf("Product of 20 and 15 is =%d\n\n",pro);
+
+    int g,h,answer;
+    g=64;
+    h=8;
+    answer=g/h;
+
+    printf("64/8 is =%d\n\n",answer);
+
+    printf("Name- lithasa jayamaha\nIndex Num- AS2023949\n\n");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
